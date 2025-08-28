c940146a4036cdb0b0ad7f0c96a61bf124035c86
cbr4l0k <juanpab.sierra@urosario.edu.co>
2025-08-28 12:45:55 +0300

    From `.astro` to `.mdx`
    Started to test with books, but need to do it with projects, and
    random... also fix the navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [mdx()],
     vite: {
-        plugins: [
-            tailwindcss(),
-            mdx()
-        ]
+        plugins: [tailwindcss()]
     },
     site: 'https://cbr4l0k.github.io',
     base: process.env.NODE_ENV === 'production' ? '/web/' : undefined,
