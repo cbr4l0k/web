ba6cc53b1c65070b84605f962c5623ff1a95b6b7
cbr4l0k <juanpab.sierra@urosario.edu.co>
2025-11-04 14:47:02 +0300

    Now I can use icons I guess 😅

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    integrations: [mdx(), icon()],
     vite: {
         plugins: [tailwindcss()]
     },
